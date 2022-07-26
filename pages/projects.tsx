ba6ea65500517808ba08c99a10119ceb3bae84e1
221B0825 <eunseoyu0825@gmail.com>
2022-07-26 23:48:35 +0900

    tailwind css

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,10 @@
+import Layout from "../components/layout";
+
 export default function () {
     return (
         //div 생략
-        <>
+        <Layout>
             <h1>프로젝트</h1>
-        </>
+        </Layout>
     )
 }
